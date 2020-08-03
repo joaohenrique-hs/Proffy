abd04e978825060d0a60b46d64ebbf19085acfe1
João Henrique <joaohenrique.hs@hotmail.com>
2020-08-03 18:04:19 -0300

    feat: Add responsive landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
+import "./assets/styles/global.css";
+
+import Landing from "./pages/Landing";
+
 function App() {
-  return <h1>Hello world</h1>;
+  return <Landing />;
 }
 
 export default App;
