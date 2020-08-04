2170328989bf0bf8cbee3dd8840bdeedd45ccae7
João Henrique <joaohenrique.hs@hotmail.com>
2020-08-03 22:08:05 -0300

    feat: List teachers on mobile first

--- web/src/pages/TecherList/index.tsx
+++ web/src/pages/TecherList/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import PageHeader from "../../components/PageHeader";
 
+import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
+
 import "./styles.css";
 
 function TeacherList() {
@@ -23,6 +25,42 @@ function TeacherList() {
           </div>
         </form>
       </PageHeader>
+
+      <main>
+        <article className="teacher-item">
+          <header>
+            <img
+              src="https://avatars1.githubusercontent.com/u/53794049?s=460&u=04c69018a6137066fea5b67141148fbb9e3c3fda&v=4"
+              alt="João Henrique"
+            />
+
+            <div>
+              <strong>João Henrique</strong>
+              <span>Informática</span>
+            </div>
+          </header>
+
+          <p>
+            O trecho padrão original de Lorem Ipsum, usado desde o século XVI,
+            está reproduzido abaixo para os interessados.
+            <br />
+            Seções 1.10.32 e 1.10.33 de "de Finibus Bonorum et Malorum" de
+            Cicero também foram reproduzidas abaixo em sua forma exata original,
+            acompanhada das versões para o inglês da tradução feita por H.
+            Rackham em 1914.
+          </p>
+
+          <footer>
+            <p>
+              Preço por hora <strong>R$ 50,00</strong>
+            </p>
+            <button type="button">
+              <img src={whatsappIcon} alt="Whatsapp" />
+              Entrar em contato
+            </button>
+          </footer>
+        </article>
+      </main>
     </div>
   );
 }
