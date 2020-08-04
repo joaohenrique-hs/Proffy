ec7eb8b2c8037fdb9bd9882e6d86a4071a4f417a
João Henrique <joaohenrique.hs@hotmail.com>
2020-08-04 17:01:57 -0300

    feat(route): Create class route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get("/users", (_request, response) => {
-  return response.json({ message: "Hello world" });
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
