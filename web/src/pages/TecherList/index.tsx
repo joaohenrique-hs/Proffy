056e232dd9b97ef249b449017363ac6de77e176c
João Henrique <joaohenrique.hs@hotmail.com>
2020-08-06 10:10:47 -0300

    feat(component): Componentize input

--- web/src/pages/TecherList/index.tsx
+++ web/src/pages/TecherList/index.tsx
@@ -1,28 +1,20 @@
-import React from "react";
+import React from 'react';
 
-import PageHeader from "../../components/PageHeader";
+import PageHeader from '../../components/PageHeader';
 
-import TeacherItem from "../../components/TeacherItem";
+import TeacherItem from '../../components/TeacherItem';
 
-import "./styles.css";
+import './styles.css';
+import Input from '../../components/Input';
 
 function TeacherList() {
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os proffys disponíveis.">
         <form id="search-teachers">
-          <div className="input-block">
-            <label htmlFor="subject">Materia</label>
-            <input type="text" id="subject" />
-          </div>
-          <div className="input-block">
-            <label htmlFor="week-day">Dia da semana</label>
-            <input type="text" id="week-day" />
-          </div>
-          <div className="input-block">
-            <label htmlFor="time">Hora</label>
-            <input type="text" id="time" />
-          </div>
+          <Input name="subject" label="Materia" />
+          <Input name="week_day" label="Dia da semana" />
+          <Input type="time" name="time" label="Hora" />
         </form>
       </PageHeader>
 
