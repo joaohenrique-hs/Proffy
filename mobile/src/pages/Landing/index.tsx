b5cb44d7c1d42934c7dc18586c7e06fdf1d2f7b3
João Henrique <joaohenrique.hs@hotmail.com>
2020-08-08 15:22:41 -0300

    feat(navigation): Tab navigation to study pages

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -17,6 +17,10 @@ function Landing() {
     navigate('GiveClasses');
   }
 
+  function handleNavigateToStudyPages() {
+    navigate('Study');
+  }
+
   return (
     <View style={styles.container}>
       <Image source={landingImg} style={styles.banner} />
@@ -26,13 +30,19 @@ function Landing() {
       </Text>
 
       <View style={styles.buttonsContainer}>
-        <RectButton style={[styles.button, styles.buttonPrimary]}>
+        <RectButton
+          onPress={handleNavigateToStudyPages}
+          style={[styles.button, styles.buttonPrimary]}
+        >
           <Image source={studyIcon} />
 
           <Text style={styles.buttonText}>Estudar</Text>
         </RectButton>
 
-        <RectButton onPress={handleNavigationToClassesPage} style={[styles.button, styles.buttonSecondary]}>
+        <RectButton
+          onPress={handleNavigationToClassesPage}
+          style={[styles.button, styles.buttonSecondary]}
+        >
           <Image source={giveClassesIcon} />
 
           <Text style={styles.buttonText}>Dar aulas</Text>
