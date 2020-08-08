676140ae6d0f2dd1fb1cf41d392e6265bf6029bc
João Henrique <joaohenrique.hs@hotmail.com>
2020-08-08 20:02:58 -0300

    feat(component): Componentize page header

--- mobile/src/pages/Favorites/index.tsx
+++ mobile/src/pages/Favorites/index.tsx
@@ -3,8 +3,14 @@ import { View } from 'react-native';
 
 import styles from './styles';
 
+import PageHeader from '../../components/PageHeader';
+
 function Favorites() {
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      <PageHeader title="Meus Proffys favoritos" />
+    </View>
+  );
 }
 
 export default Favorites;
