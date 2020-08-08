002ce507d25a2664f6bdf5777ebcae00befe099d
João Henrique <joaohenrique.hs@hotmail.com>
2020-08-08 00:03:13 -0300

    feat: Connect with whatsapp api
    Create connection with whatsapp api.
    Increment number of connections when contact some teacher.

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './style.css';
+import api from '../../services/api';
 
 export interface Teacher {
   id: string;
@@ -19,6 +20,12 @@ export interface TeacherItemProps {
 }
 
 const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id,
+    });
+  }
+
   return (
     <article className="teacher-item">
       <header>
@@ -37,10 +44,14 @@ const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
           Preço por hora
           <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a
+          target="_blank"
+          onClick={createNewConnection}
+          href={`https://wa.me/${teacher.whatsapp}`}
+        >
           <img src={whatsappIcon} alt="Whatsapp" />
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
